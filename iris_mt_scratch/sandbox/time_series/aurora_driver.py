d9ac911c1a69b31565f77ec3abee67ddcf22f065
Karl N. Kappler <magnetotellurics@gmail.com>
2021-05-16 12:01:54 -0700

    tidy up driver

--- iris_mt_scratch/sandbox/time_series/aurora_driver.py
+++ iris_mt_scratch/sandbox/time_series/aurora_driver.py
@@ -25,6 +25,7 @@ from iris_mt_scratch.sandbox.xml.xml_sandbox import describe_inventory_stages
 from iris_mt_scratch.sandbox.xml.xml_sandbox import get_response_inventory_from_iris
 from mt_metadata.timeseries import Experiment
 from mt_metadata.timeseries.stationxml import XMLInventoryMTExperiment
+from mt_metadata.utils import STATIONXML_02
 from mth5.mth5 import MTH5
 from mth5.timeseries.channel_ts import ChannelTS
 from mth5.timeseries.run_ts import RunTS
@@ -37,7 +38,9 @@ TEST_DATA_HELPER = TestDataHelper(dataset_id="PKD_SAO_2004_272_00-2004_272_02")
 xml_path = Path("/home/kkappler/software/irismt/mt_metadata/data/xml")
 magnetic_xml_template = xml_path.joinpath("mtml_magnetometer_example.xml")
 electric_xml_template = xml_path.joinpath("mtml_electrode_example.xml")
-single_station_xml_template = xml_path.joinpath("mtml_single_station.xml")
+single_station_xml_template = STATIONXML_02 # Fails for "no survey key"
+single_station_xml_template = Path("single_station_mt.xml")
+
 
 #<LOAD SOME DATA FROM A SINGLE STATION>
 N = 288000#86400
@@ -79,18 +82,6 @@ def get_mth5_experiment_from_iris(station_id, save_experiment_xml=False):
         print(f"saved experiement to {output_xml_path}")
     return experiment
 
-
-def get_filters_dict_from_experiment(experiment):
-    print(experiment, type(experiment))
-    surveys = experiment.surveys
-    survey = surveys[0]
-    print("Survey Filters", survey.filters)
-    survey_filters = survey.filters
-    filter_keys = list(survey_filters.keys())
-    print("FIlter keys", filter_keys)
-    for filter_key in filter_keys:
-        print(filter_key, survey_filters[filter_key])
-    return survey_filters
 
 def embed_metadata_into_run_ts(station_id, xml_path=None):
     """
@@ -172,27 +163,51 @@ def get_example_data(component=None, load_actual=False, station_label=None):
 
 #</LOAD SOME DATA FROM A SINGLE STATION>
 
+def set_driver_parameters():
+    driver_parameters = {}
+    driver_parameters["create_xml"] = True
+    driver_parameters["test_filter_control"] = True
+    driver_parameters["runts_from_xml"] = True
+    driver_parameters["initialize_data"] = True
+    return driver_parameters
+
 
 def main():
-    #experiment = get_mth5_experiment_from_iris("PKD", save_experiment_xml=True)
-    #experiment = get_mth5_experiment_from_iris("SAO", save_experiment_xml=True)
-    filter_control_example()
-    filter_control_example(xml_path=get_station_xml_filename("PKD"))
-    #runts_object = embed_metadata_into_run_ts("REW09", xml_path=Path("single_station_mt.xml"))
+    driver_parameters = set_driver_parameters()
+    #<CREATE METADATA XML>
+    if driver_parameters["create_xml"]:
+        experiment = get_mth5_experiment_from_iris("PKD", save_experiment_xml=True)
+        experiment = get_mth5_experiment_from_iris("SAO", save_experiment_xml=True)
+    #</CREATE METADATA XML>
 
-    #<FAILS no SURVEY>
-    pkd_xml = get_station_xml_filename("PKD")
-    runts_object = embed_metadata_into_run_ts("PKD", xml_path=pkd_xml)
-    #runts_object = embed_metadata_into_run_ts("PKD", xml_path="tmp.xml")
-    #</FAILS no SURVEY>
+    #<TEST FILTER CONTROL>
+    if driver_parameters["test_filter_control"]:
+        filter_control_example()
+        filter_control_example(xml_path=get_station_xml_filename("PKD"))
+    #</TEST FILTER CONTROL>
 
-    runts_object = embed_metadata_into_run_ts("PKD")
-    runts_object = embed_metadata_into_run_ts(xml_path=Path("single_station_mt.xml"))
-    pkd_mvts = get_example_data(station_label="PKD")
-    sao_mvts = get_example_data(station_label="SAO")
-    pkd = pkd_mvts.dataset
-    sao = sao_mvts.dataset
-    pkd.update(sao)
+
+
+    #<TEST RunTS FROM XML>
+    if driver_parameters["runts_from_xml"]:
+        runts_object = embed_metadata_into_run_ts("PKD")
+
+        pkd_xml = get_station_xml_filename("PKD")
+        runts_object = embed_metadata_into_run_ts("PKD", xml_path=pkd_xml)
+
+
+        runts_object = embed_metadata_into_run_ts("REW09",
+                                                 xml_path=single_station_xml_template)
+    #</TEST RunTS FROM XML>
+
+    #<INITIALIZE DATA>
+    if driver_parameters["initialize_data"]:
+        pkd_mvts = get_example_data(station_label="PKD")
+        sao_mvts = get_example_data(station_label="SAO")
+        pkd = pkd_mvts.dataset
+        sao = sao_mvts.dataset
+        pkd.update(sao)
+    #</INITIALIZE DATA>
     print("try to combine these runs")
 
 
